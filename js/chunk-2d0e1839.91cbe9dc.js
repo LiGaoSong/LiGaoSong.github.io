1d660606716591864931192a0ed801838124aa19
LiGaoSong <34428829+LiGaoSong@users.noreply.github.com>
2022-05-10 23:05:42 +0800

    two

---
+++ js/chunk-2d0e1839.91cbe9dc.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0e1839"],{"7b80":function(n,e,i){"use strict";i.r(e);var t=i("7a23");function a(n,e,i,a,c,o){return Object(t["y"])(),Object(t["f"])("div",null,"wo shi liebiao")}var c={name:"Detail"},o=i("6b0d"),r=i.n(o);const u=r()(c,[["render",a]]);e["default"]=u}}]);
+//# sourceMappingURL=chunk-2d0e1839.91cbe9dc.js.map
