8b35646eb81e4f6cfc81bd362b2cfa4e2286a533
LiGaoSong <34428829+LiGaoSong@users.noreply.github.com>
2022-06-23 00:12:33 +0800

    add new

---
+++ js/chunk-2d0da39c.4ea42619.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0da39c"],{"6b60":function(n,e,c){"use strict";function r(n,e,c,r,t,a){return" 123 "}c.r(e);var t={name:"SingerDetail"},a=c("6b0d"),d=c.n(a);const i=d()(t,[["render",r]]);e["default"]=i}}]);
+//# sourceMappingURL=chunk-2d0da39c.4ea42619.js.map
